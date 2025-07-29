06c5a22f2df504588156a2fae2cd6bf058f3e5e7
Kaiky Souza <148717444+KaikySsouza@users.noreply.github.com>
2025-07-29 16:29:32 -0300

    feat:tela de login

--- taskflow/src/pages/login/Login.jsx
+++ taskflow/src/pages/login/Login.jsx
@@ -1,10 +1,36 @@
 function Login() {
-    return(
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-[#599DC7] w-[83.4vh] h-[83.4vh] ">
+        <img className="relative bottom-[80px]"
+          src="/TaskFlowlogo.png"
+          alt="taskflow.png"
+          width={250}
+        />
+        <h1>Bem-vindo(a)!</h1>
+        <p>Conecte-se e continue criando explorando e evoluindo com a gente.</p>
+      </div>
+
+      <div className="bg-[#F4F4F4] w-[83.4vh] h-[83.4vh]">
+        <h2>Login</h2>
+
         <div>
-            <img src="/TaskF
-            lowLogo.png" alt="taskflow.png" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Senha" />
+
+          <div>
+            <input type="checkbox" />
+            <p>Lembre-se</p>
+            <div>
+              <p>recuperar senha</p>
+            </div>
+          </div>
         </div>
-    )
+
+        <button>Login</button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
