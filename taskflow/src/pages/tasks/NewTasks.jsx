e1f63979e932f5202f953475ef8a80379765e787
Kaiky Souza <148717444+KaikySsouza@users.noreply.github.com>
2026-03-04 16:07:10 -0300

    feat: Criado modal para criação de tarefas e estilizado.

--- taskflow/src/pages/tasks/NewTasks.jsx
+++ taskflow/src/pages/tasks/NewTasks.jsx
@@ -1,5 +1,19 @@
-export default function NewTasks() {
+import { X } from "lucide-react"
+import { useState } from "react"
+export default function NewTasks({ viewtask }) {
+
+    const [hide, setHide] = useState(false)
+
     return(
-        <h1></h1>
+        <div className={`bg-[#fff] p-[10vh] w-[100%] max-w-[120vh] rounded-2xl ${hide ? 'transition-all duration-700 ml-[100%] opacity-0' : 'opacity-1000 '}`} >
+        <X  onClick={() => setHide(viewtask)} className={`transition-all duration-700 flex relative ml-[100%] bottom-10 left-[5%]`}/> 
+        <div className="flex flex-col space-y-4 my-3">
+        <h1 className="text-center text-[25px]">Nova Tarefa</h1>
+        <input className="bg-cyan-100 p-2 rounded-2xl" placeholder="Tarefa:" type="text" />
+        <input className="bg-cyan-100 p-2 rounded-2xl" placeholder="Descrição" type="text" />
+        <input className="bg-cyan-100 p-2 rounded-2xl" type="datetime-local" />
+        <button  o className="bg-cyan-100 p-2 w-[25vh]  m-auto rounded-2xl hover:bg-green-500" >Criar</button>
+        </div>
+        </div>
     )
 }
--- taskflow/src/pages/tasks/Tasks.jsx
+++ taskflow/src/pages/tasks/Tasks.jsx
@@ -10,14 +10,14 @@ export default function Tasks() {
       title: "Apreender React",
       description: "Estudar todos os dias os conceitos de hooks e props",
       term: "10/10/2005 12:00:00",
-      finish: "false",
+      finish: false,
     },
     {
       id: 2,
       title: "Praticar JavaScript",
       description: "Resolver exercícios de lógica e manipulação de arrays",
       term: "15/10/2005 18:00:00",
-      finish: "false",
+      finish: false,
     },
     {
       id: 3,
@@ -25,56 +25,121 @@ export default function Tasks() {
       description:
         "Desenvolver um projeto utilizando HTML, CSS e JavaScript puro",
       term: "20/10/2005 20:00:00",
-      finish: "false",
+      finish: false,
     },
     {
       id: 4,
       title: "Estudar APIs",
       description: "Aprender a consumir APIs utilizando fetch e async/await",
       term: "25/10/2005 19:00:00",
-      finish: "false",
+      finish: false,
     },
   ]);
 
+  const [modal, setModal] = useState(false)
+  const [view, setTask] = useState(false)
+  
+
   console.log(tasks);
 
+ // Função responsavel por validar qual task foi clicada.
+ 
+ function ViewClick(taskId) {
+  const newtask = tasks.map((task) => {
+    if(task.id === taskId) {
+      return {...task, finish: !task.finish}
+    }else{
+      return task
+    }
+  })
+  setTasks(newtask)
+ }
 
+// Nova task
 
+ function NewTask(title, description, term) {
+  const newtask = {
+    id: tasks.length + 1,
+    title, 
+    description, 
+    term,
+    finish: false
+  }
+  setTasks(...tasks, newtask)
+}
+  
+
+ // Excluir task
+
+ function DeleteTask(taskId) {
+   const del  =  tasks.filter(task => task.id !== taskId)
+    setTasks(del)
+  }
+
+// Resposavel por mostrar todas as tasks
 
   const salvar = tasks.map((task) => {
     return (
      
-      <ul className="flex ">
-        <li className="bg-cyan-100 m-2 p-2 w-100 text-start rounded-2xl cursor-pointer ">{task.title}</li>
+      <ul key={tasks.id} className="flex ">
+        <li onClick={() => ViewClick(task.id)} className="bg-cyan-100 m-2 p-2 w-[70vh] text-start rounded-2xl cursor-pointer ">{task.title}</li>
+        <button className="bg-cyan-100 p-2 m-2 w-[20vh] rounded-2xl cursor-pointer">Visualizar tarefa</button>
        <button className="bg-cyan-100 p-2 m-2 w-[15vh] rounded-2xl cursor-pointer">Editar</button>
-       <button className="cursor-pointer p-2"><Trash className=""/></button>
+       <button onClick={() =>  DeleteTask(task.id)} className="cursor-pointer p-2"><Trash className=""/></button>
       </ul>
    
 
     );
-  }); // Possivel adicionar isso dentro do reder.
+  }); 
 
+  // Fechar o modal.
 
+  function ViewTask() {
+    if(view != modal){
+      return setModal(false)
+    }else{
+      console.log('deu errado')
+    }
+  }
+  
+ 
+  // Possivel adicionar isso dentro do reder.
 
   return (
     <div className=" bg-gray-300 h-screen">
+      
       <Navbar />
 
-      <header className="flex text-center gap-3 justify-center cursor-pointer my-5">
-        <h1 className="text-[30px] ">Criar tarefa</h1>
-        <button className="">
+      {/* Esconder Tarefa */}
+
+     
+
+      {/* Criar tasks */}
+      <header className="flex ">
+        <div className={`bg flex text-center m-auto gap-3 justify-center cursor-pointer my-5   ${modal ? 'opacity-0 transition-all duration-400' : ''}`}>
+        <h1 className="text-[30px]">Criar tarefa</h1>
+        <button onClick={() => setModal(true)}>
           <FilePlusCorner/>
         </button>
+        </div>
+      
       </header>
-
-      <div className="flex flex-col m-auto  bg-[#fff] w-[80vh] rounded-2xl ">
-        <div className="flex justify-around gap-[55vh] my-2">
+        <div className={` m-auto  bg-[#fff] w-[100%] max-w-[120vh] rounded-2xl  ${modal ? 'opacity-0 pointer-events-none transition-all duration-700 ' : 'opacity-1000  '} `  } >
+         
+        <div className="flex justify-around gap-[90vh] my-2">
         <h2 className="text-[25px]">Tarefas</h2>
         <p className="mt-3 hover:underline">Ver mais</p>
         </div>
-        {salvar}
-        <NewTasks />
+        {salvar}  
+        
+        {/* Motrar tasks */}
+        
       </div>
+
+
+      {/* Responsavel por mostrar e esconder o modal */}    
+      <div className={`flex absolute inset-0 justify-center items-center  my-4  w-[50%] max-w-[120vh] rounded-2xl   ${modal ? 'opacity-1000 transition-all duration-1000 m-[25%] ' : 'opacity-0 pointer-events-none  transition-all duration-700' }`}> <NewTasks viewtask={ViewTask}/> </div>
+      
     </div>
   );
 }
